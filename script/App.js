92ee05548b7ba494dd329da726b6f32e864a208a
TheProShizer <marcin.siek20@gmail.com>
2023-05-17 20:42:35 +0200

    aktualizacja

---
+++ script/App.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-type-text", {
+    strings: ["Botów Discord", "Stron WWW", "Cs 1.6, Cs:Go", "TeamSpeak 3"],
+    typeSpeed: 160,
+    backSpeed: 50,
+    loop: true,
+  });
